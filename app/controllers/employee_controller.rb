4ada2dfef4e589873d1ac614f0dbb915e80affa0
ShabiniRajadas <shabiniraja90@gmail.com>
2013-11-29 11:02:28 +0530

    Comments

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,5 +1,6 @@
 class EmployeeController < ApplicationController
 
+  # list out the employees in the index page
   def index
     @user=User.order("user_name").page(params[:page]).per(1)
     @search = Employee.search(params[:q])
@@ -7,16 +8,18 @@ class EmployeeController < ApplicationController
     @user = @use.order("id").page(params[:page]).per(2)
   end
 
+  #list out the hr's in the user's list
   def hr
   	@user=User.all
   end
 
+  #list out the managers in the user's list
   def manager
   @user=User.find(session['user_id'])
   @employee=@user.employee.subordinates
-    
   end
 
+  #list out the employees(common users)
   def employee
   end
 
